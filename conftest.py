15e38ba358258c64c03533f00db3895c0dc1f695
YaroslavOleksiienko <yaroslavolexeenko.qa@gmail.com>
2022-09-14 13:22:29 +0300

    Initial commit

---
+++ conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose language: es or fr")
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("language")
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+        browser.get(f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+        browser.get(f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    yield browser
+    browser.quit()
